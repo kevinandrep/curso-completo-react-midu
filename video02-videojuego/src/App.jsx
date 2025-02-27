78ef3fb667b373031fda21c09ac7779eb7f87b58
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2025-02-27 12:11:22 -0500

    seteando al ganador

--- video02-videojuego/src/App.jsx
+++ video02-videojuego/src/App.jsx
@@ -5,6 +5,17 @@ const TURNS = {
   O: 'o'
 }
 
+const WINNER_COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],
+]
+
 
 
 const Square = ({children, isSelected, updateBoard, index}) => {
@@ -22,18 +33,44 @@ const Square = ({children, isSelected, updateBoard, index}) => {
 }
 
 function App() {
-
+  //variables de estado para tablero y turno
   const [board, setBoard] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState((TURNS.X))
+  const [winner, setWinner] = useState(null)
 
+  //actualizando el tablero
   const updateBoard = (index) => {
+    //evitando volver a colocar valor en el square
+    if (board[index] || winner) return
+
+    //actualizando el valor del  tablero
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
     
-
+    //cambiando  de turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+
+    //chequeando si existe nuevo ganador al terminar el turno
+    const newWinner = checkWinner(newBoard)
+
+    if (newWinner) {
+      //alert(`El ganador es ${newWinner}`)
+      setWinner(prevWinner => newWinner)
+    }
+
+  }
+
+  //CHEQUEANDO EL WINNER
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS){
+      const [a,b,c] = combo;
+      if (boardToCheck[a] && boardToCheck[a] === boardToCheck[b] && boardToCheck[a] === boardToCheck[c]) {
+        return boardToCheck[a]
+      }
+    }
+    return null
   }
 
 
