f594e6fc32f11ae3fa010956e76139162c86df38
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2025-02-28 18:54:24 -0500

    mouse follow con evento y suscripcion y remove sub

--- 02-mousefollower/src/App.jsx
+++ 02-mousefollower/src/App.jsx
@@ -1,9 +1,49 @@
 
+import { useState } from "react";
+import { useEffect } from "react";
+
 function App() {
+
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({x:0, y:0});
+
+  useEffect(() => {
+    console.log('efecto');
+
+    const handleMove = (event) => {
+      const {clientX, clientY} = event
+      setPosition({x: clientX, y: clientY})
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+     window.removeEventListener('pointermove', handleMove)
+    }
+    
+  }, [enabled])
 
   return (
     <>
-     fdafas
+     <div style={{
+      position: 'absolute',
+      backgroundColor: '#59f',
+      borderRadius: '50%',
+      opacity: 0.8,
+      pointerEvents: 'none',
+      left: -20,
+      top: -20,
+      width: 40,
+      height: 40,
+      transform: `translate(${position.x}px, ${position.y}px)`
+     }}>
+
+     </div>
+      <button onClick={() => setEnabled(prevEnabled => !prevEnabled)}>
+      {enabled ? 'Desactivar' : 'Activar'}
+      </button>
     </>
   )
 }
