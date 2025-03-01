0aa0f23913b141990aa80901b25104aab835ec6e
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2025-03-01 17:45:01 -0500

    separando 2 useEffect

--- react-prueba-tecnica/src/App.jsx
+++ react-prueba-tecnica/src/App.jsx
@@ -5,28 +5,33 @@ const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`
 
 export function App () {
-  const [fact, setFact] = useState('Loading')
+  const [fact, setFact] = useState()
   const [imageUrl, setImageUrl] = useState()
 
+  // efecto para recuperar la cita textual de la api al cargar
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then(res => res.json())
       .then(data => {
         const { fact } = data
         setFact(fact)
-
-        const threeFirstWords = fact.split(' ', 3).join(' ')
-        console.log(threeFirstWords)
-
-        fetch(`https://cataas.com/cat/says/${threeFirstWords}?fontSize=50&fontColor=red&json=true`)
-          .then(res => res.json())
-          .then(response => {
-            const { url } = response
-            setImageUrl(url)
-          })
       })
   }, [])
 
+  // efecto para recuperar cada vez que se tenga una cita nueva
+
+  useEffect(() => {
+    if (!fact) return
+    const threeFirstWords = fact.split(' ', 3).join(' ')
+    console.log(threeFirstWords)
+
+    fetch(`https://cataas.com/cat/says/${threeFirstWords}?fontSize=50&fontColor=red&json=true`)
+      .then(res => res.json())
+      .then(response => {
+        const { url } = response
+        setImageUrl(url)
+      })
+  }, [fact])
   return (
     <main>
       <h1>App de gatitos</h1>
