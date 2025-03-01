4f0f87e34c4c701f7ce51e348f89c08a7a38149e
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2025-03-01 17:33:29 -0500

    fetch de las apis funcional con css funcional

--- react-prueba-tecnica/src/App.jsx
+++ react-prueba-tecnica/src/App.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`
+
 export function App () {
+  const [fact, setFact] = useState('Loading')
+  const [imageUrl, setImageUrl] = useState()
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+
+        const threeFirstWords = fact.split(' ', 3).join(' ')
+        console.log(threeFirstWords)
+
+        fetch(`https://cataas.com/cat/says/${threeFirstWords}?fontSize=50&fontColor=red&json=true`)
+          .then(res => res.json())
+          .then(response => {
+            const { url } = response
+            setImageUrl(url)
+          })
+      })
+  }, [])
+
   return (
-    <h1>Mi app</h1>
+    <main>
+      <h1>App de gatitos</h1>
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img src={imageUrl} alt='cat' />}
+    </main>
   )
 }
