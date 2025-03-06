363c0d6a48adfcca1f02f371f8b1528b8de79cc3
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2025-03-06 10:47:50 -0500

    refactorizando el componente movies con 3 sub componentes

--- 05-react-buscador-peliculas/src/App.jsx
+++ 05-react-buscador-peliculas/src/App.jsx
@@ -1,17 +1,10 @@
 import './App.css'
 import responseMovies from './mocks/with-results.json'
-import withoutResults from './mocks/no-results.json'
+import { Movies } from './components/Movies';
 
 function App() {
 
   const movies = responseMovies.Search
-  const hasMovies = movies?.length > 0
-
-  console.log(responseMovies);
-  
-  console.log(movies);
-
-  
 
   return (
     <div>
@@ -26,18 +19,7 @@ function App() {
       <main>
         Aqui iran los resultados
         {
-          hasMovies ?
-            (
-              <ul>
-                {movies.map(movie => (
-                  <li key={movie.imdbID}>
-                    <h3>{movie.Title}</h3>
-                    <p>{movie.Year}</p>
-                    <img src={movie.Poster} alt={movie.Title}></img>
-                  </li>
-                ))}
-              </ul>
-            ): <p>No se encontraron resultados</p>
+          <Movies movies={movies}></Movies>
         }
       </main>
     </div>
---
+++ 05-react-buscador-peliculas/src/components/Movies.jsx
@@ -0,0 +1,32 @@
+
+
+function ListOfMovies ({movies}) {
+    return (
+        <ul>
+        {movies.map(movie => (
+            <li key={movie.imdbID}>
+            <h3>{movie.Title}</h3>
+            <p>{movie.Year}</p>
+            <img src={movie.Poster} alt={movie.Title}></img>
+            </li>
+        ))}
+        </ul>
+    )
+}
+
+function NoMoviesResult () {
+    return (
+        <p>No se encontraron peliculas</p>
+    )
+}
+
+export function Movies ({movies}) {
+    
+    const hasMovies = movies?.length > 0
+
+    return (
+        hasMovies ? 
+            <ListOfMovies movies={movies}></ListOfMovies>
+            :<NoMoviesResult></NoMoviesResult>
+    )
+}
