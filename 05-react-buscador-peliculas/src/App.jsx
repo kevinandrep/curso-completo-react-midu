796646a5dbeca7463438633c022402eff86bebf0
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2025-03-06 10:30:13 -0500

    funcionalidad copiando json a archivos locales

--- 05-react-buscador-peliculas/src/App.jsx
+++ 05-react-buscador-peliculas/src/App.jsx
@@ -1,6 +1,17 @@
 import './App.css'
+import responseMovies from './mocks/with-results.json'
+import withoutResults from './mocks/no-results.json'
 
 function App() {
+
+  const movies = responseMovies.Search
+  const hasMovies = movies?.length > 0
+
+  console.log(responseMovies);
+  
+  console.log(movies);
+
+  
 
   return (
     <div>
@@ -14,6 +25,20 @@ function App() {
 
       <main>
         Aqui iran los resultados
+        {
+          hasMovies ?
+            (
+              <ul>
+                {movies.map(movie => (
+                  <li key={movie.imdbID}>
+                    <h3>{movie.Title}</h3>
+                    <p>{movie.Year}</p>
+                    <img src={movie.Poster} alt={movie.Title}></img>
+                  </li>
+                ))}
+              </ul>
+            ): <p>No se encontraron resultados</p>
+        }
       </main>
     </div>
   )
